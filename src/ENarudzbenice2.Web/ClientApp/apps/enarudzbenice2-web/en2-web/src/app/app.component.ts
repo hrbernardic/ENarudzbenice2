633462b624e132ca766d9ca124077eeb27336f25
Hrvoje Bernardić <bernardic.hrvoje@gmail.com>
2018-11-28 21:12:39 +0100

    nrwl initial commit

---
+++ src/ENarudzbenice2.Web/ClientApp/apps/enarudzbenice2-web/en2-web/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'en2-root',
+  templateUrl: './app.component.html',
+  styles: []
+})
+export class AppComponent {
+  title = 'enarudzbenice2-web-en2-web';
+}
